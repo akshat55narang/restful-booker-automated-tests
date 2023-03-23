7ae95139103a8a4b70b84688c5d53cfe3a8c1382
Akshat <akshat55narang@gmail.com>
2023-03-23 20:06:38 +0100

    added interface and its implementing class

---
+++ src/main/kotlin/api/RequestSenderImpl.kt
@@ -0,0 +1,51 @@
+package api
+
+import io.restassured.module.kotlin.extensions.Given
+import io.restassured.module.kotlin.extensions.When
+import io.restassured.response.Response
+import io.restassured.specification.RequestSpecification
+
+open class RequestSenderImpl: RequestSender {
+    override fun get(requestSpecification: RequestSpecification, path: String): Response =
+        Given {
+            spec(requestSpecification)
+        } When {
+            get(path)
+        }
+
+
+    override fun post(requestSpecification: RequestSpecification, path: String): Response =
+        Given {
+            spec(requestSpecification)
+        } When {
+            post(path)
+        }
+
+    override fun put(requestSpecification: RequestSpecification, path: String): Response =
+        Given {
+            spec(requestSpecification)
+        } When {
+            put(path)
+        }
+
+    override fun patch(requestSpecification: RequestSpecification, path: String): Response =
+        Given {
+            spec(requestSpecification)
+        } When {
+            patch(path)
+        }
+
+    override fun delete(requestSpecification: RequestSpecification, path: String): Response =
+        Given {
+            spec(requestSpecification)
+        } When {
+            delete(path)
+        }
+
+    override fun options(requestSpecification: RequestSpecification, path: String): Response =
+        Given {
+            spec(requestSpecification)
+        } When {
+            options(path)
+        }
+}
