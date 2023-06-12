b8c0309fc383282b021b8dcb4a2cc15d039f98a4
Akshat <akshat55narang@gmail.com>
2023-06-12 23:37:40 +0200

    added logging config

--- src/main/kotlin/api/RequestSenderImpl.kt
+++ src/main/kotlin/api/RequestSenderImpl.kt
@@ -1,18 +1,31 @@
 package api
 
+import io.restassured.RestAssured
+import io.restassured.RestAssured.config
+import io.restassured.config.LogConfig
+import io.restassured.config.RestAssuredConfig
 import io.restassured.module.kotlin.extensions.Given
 import io.restassured.module.kotlin.extensions.When
 import io.restassured.response.Response
 import io.restassured.specification.RequestSpecification
 
 open class RequestSenderImpl: RequestSender {
+
+    init {
+        val loggingConfig = LogConfig.logConfig()
+            .blacklistHeaders(mutableListOf("Cookie", "Authorization"))
+            .enableLoggingOfRequestAndResponseIfValidationFails()
+
+        config = config().logConfig(loggingConfig)
+
+    }
+
     override fun get(requestSpecification: RequestSpecification, path: String): Response =
         Given {
             spec(requestSpecification)
         } When {
             get(path)
         }
-
 
     override fun post(requestSpecification: RequestSpecification, path: String): Response =
         Given {
