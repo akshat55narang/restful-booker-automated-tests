5232a34a1cc51e3da53ca649d0644f7c71001d3c
Akshat <akshat55narang@gmail.com>
2023-06-12 23:35:54 +0200

    added requestSpec to accept Either header token or user role for token gen

--- src/main/kotlin/api/BaseRequestSpecProvider.kt
+++ src/main/kotlin/api/BaseRequestSpecProvider.kt
@@ -1,21 +1,35 @@
 package api
 
 import ConfigManager.getBaseUri
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import auth.TokenFactory.BASIC_AUTH_HEADER_TOKEN
+import auth.TokenFactory.getTokenFor
 import io.restassured.builder.RequestSpecBuilder
-import io.restassured.filter.log.RequestLoggingFilter
-import io.restassured.filter.log.ResponseLoggingFilter
 import io.restassured.specification.RequestSpecification
+import types.BasicAuthToken
+import types.UserRole
 
-abstract class BaseRequestSpecProvider(private val baseUri: String = getBaseUri(), private val basePath: String) : RequestSenderImpl() {
+abstract class BaseRequestSpecProvider(private val baseUri: String = getBaseUri(), private val basePath: String) :
+    RequestSenderImpl() {
 
-    fun baseRequestSpecificationWithoutToken(): RequestSpecification = RequestSpecBuilder()
+    fun requestSpecWithoutAuthHeader(): RequestSpecification = RequestSpecBuilder()
         .setBaseUri(baseUri)
         .setBasePath(basePath)
         .setRelaxedHTTPSValidation()
-        .addFilter(RequestLoggingFilter())
-        .addFilter(ResponseLoggingFilter())
         .build()
 
-    protected fun baseRequestSpecificationWithToken(token: String): RequestSpecification = baseRequestSpecificationWithoutToken()
-        .header("Cookie", "token=$token")
+    protected fun requestSpec(
+        authMechanism: Either<BasicAuthToken, UserRole> = UserRole.ADMIN.right()
+    ): RequestSpecification {
+        val requestSpecification = requestSpecWithoutAuthHeader()
+        return authMechanism.fold(
+            {
+                if (it.value != null) requestSpecification.header("Authorization", "Basic $BASIC_AUTH_HEADER_TOKEN")
+                else requestSpecification
+            },
+            { requestSpecification.header("Cookie", "token=${getTokenFor(userRole = it)}") }
+        )
+    }
 }
